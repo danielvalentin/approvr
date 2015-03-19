22f23db4b2d7cfdf364d86ba4f3e816d84474406
Sakto <saktomail@gmail.com>
2015-03-19 17:14:19 +0100

    .

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -72,7 +72,40 @@
 							</li>
 						<?php else: ?>
 							<li>
-								<a href="<?php echo Site::route('user.login'); ?>"><?php echo trans('buttons.login'); ?></a>
+								<div class="m__userForm">
+									<h2 class="m__userForm-header">Log in</h2>
+									<form role="form" action="<?php echo Site::route('user.login'); ?>" method="post" class="loginForm">
+										<input type="hidden" name="_token" value="{{ csrf_token() }}">
+										
+										<label for="email" class="m__userForm-label">Email</label>
+										<div class="input-group">
+											<span class="input-group-addon" id="basic-addon2"><span class="glyphicon glyphicon-user" aria-hidden="true"></span></span>
+											<input type="text" type="email" value="{{ old('email') }}" placeholder="<?php echo trans('forms.placeholders.email'); ?>" id="auth-email" name="email" class="form-control" aria-describedby="basic-addon2">
+										</div>
+										
+										<label for="password" class="m__userForm-label">Password</label>	
+										<div class="input-group">
+											<span class="input-group-addon" id="basic-addon2"><span class="glyphicon glyphicon-lock" aria-hidden="true"></span></span>
+											<input type="password" id="auth-password" placeholder="<?php echo trans('forms.placeholders.password'); ?>" value="{{ old('email') }}"  name="password" class="form-control" aria-describedby="basic-addon2">
+										</div>
+
+
+										<div class="m__userForm-footer">
+											<div class="m__userForm-footerItem">
+												<label>
+													<input type="checkbox" name="remember"<?php echo (old('remember')=='yes'?' checked="checked"':'') ?> value="yes" /> <?php echo trans('forms.labels.staylogged'); ?>
+												</label>
+											</div>
+
+											<div class="m__userForm-footerItem">
+												<button class="btn btn-primary"><?php echo trans('buttons.login'); ?></button>
+											</div>
+										</div> <!-- end m__footer !-->
+										<div class="m__userForm-register">
+											<a href="<?php echo trans('routes.user.signup'); ?>" title="<?php echo trans('forms.login.signup'); ?>">Don't have an account?</a>
+										</div>
+									</form>
+								</div>
 							</li>
 						<?php endif; ?>
 					</ul>
